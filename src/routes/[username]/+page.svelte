791be906e6e8980eccc20252ede225949e87dd5d
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-08-01 13:40:54 -0500

    User Display Page
    Navigating to /[username] displays information (username, profile pic, bio, etc.)

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -3,3 +3,34 @@
     
     export let data: PageData;
 </script>
+
+<!-- 
+    'svelte:head' allows us to add data to the head of the html document, like title and meta tags.
+    Good for SEO and adding Twitter cards and whatnot     
+-->
+<svelte:head>
+    <title>@{data.username} Links</title>
+    <meta name="description" content={data.bio} />
+</svelte:head>
+
+<main class="prose text-center mx-auto mt-8">
+
+    <h1 class="text-7xl text-purple-500">
+        @{data.username}
+    </h1>
+
+    <img
+        src={data.photoURL ?? "/user.png"}
+        alt="photoURL"
+        width="256"
+        class="mx-auto"
+    />
+
+    <p class="text-xl my-8">{data.bio ?? "no bio yet..."}</p>
+
+    <ul class="list-none">
+        {#each data.links as item}
+            {@debug item}
+        {/each}
+    </ul>
+</main>
