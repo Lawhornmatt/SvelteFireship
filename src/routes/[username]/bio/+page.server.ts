fb1a0773be3355195cf38a4fbae53f2613b722a4
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-08-02 11:39:25 -0500

    Bio Update
    In [username]/bio one can update and change their bio. Submissions are handled on the server and feedback on if the submitted bio is too long relayed back to the fontend.

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,36 +1,13 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { adminAuth, adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import { error, redirect, fail } from "@sveltejs/kit";
 
-/* OLD METHOD: 
+/* OLD LOAD METHOD: 
     Allowed access to this specific route via verifying the stored session cookie
     But this was specific to this route, so it was refactored 
     to be usable on any server end-point via svelte hooks
-
-export const load = (async ({ cookies }) => {    
-    // retrieve the session cookie
-    const sessionCookie = cookies.get('__session');
-    try {
-        // Use the firebase SDK's adminAuth to verify the cookie
-        const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!);
-        // Then fetch the user data from the firestore db
-        const userDoc = await adminDB.collection('user').doc(decodedClaims.uid).get();
-        const userData = userDoc.data();
-
-        // Return the user data, allowing us to use it in the page.svelte file
-        return {
-            bio: userData?.bio,
-        }
-    } catch (e) {
-        console.log(e);
-        //redirect(301, '/login);
-        throw error(401, 'Unauthorized request!');
-    };
-}) satisfies PageServerLoad;
+   Load Post-refactor:
 */
-
-
-// Post-refactor
 export const load = (async ({ locals, params }) => {
 
     // Gets the user id via Locals event object instead of directly from cookie
@@ -53,3 +30,39 @@ export const load = (async ({ locals, params }) => {
         bio,
     };
 }) satisfies PageServerLoad;
+
+
+export const actions = {
+
+    // If you only have one form on the page, you likely only need one action, which should be name default
+    // It has access to all the same obj as we did in data fetching
+    default: async ({ locals, request, params }) => {
+
+        // grab uid from locals just like we did on load
+        const uid = locals.userID;
+
+        // grab form data from page.svelte
+        const data = await request.formData();
+        const bio = data.get('bio');
+
+        // find the user doc using uid and update the bio information
+        const userRef = adminDB.collection("users").doc(uid!);
+        const { username } = (await userRef.get()).data()!;
+
+        // in actions we can do some validation like:
+        // ...is this your username?
+        if (params.username !== username) {
+            throw error(401, "That username does not belong to you");
+        };
+        // ...are you submitting too long of a bio?
+        if (bio!.length > 260) {
+            // This doesnt send us to a whole new error page, instead it updates the page store with a page.problem object
+            return fail(400, { problem: "Bio must be less than 260 characters" });
+        };
+
+        // If validation doesnt throw err, we update the doc on the db
+        await userRef.update({
+            bio,
+        });
+    },
+} satisfies Actions;
--- src/routes/[username]/bio/+page.svelte
+++ src/routes/[username]/bio/+page.svelte
@@ -1,7 +1,50 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
+    import type { PageData } from "./$types";
+    import { page } from "$app/stores";
+    import { enhance } from "$app/forms";
     export let data: PageData;
 </script>
 
-<h1>Edit Bio</h1>
+<main class="max-w-lg prose text-center mx-auto my-6">
+    <p>Current Bio: <span class="text-info">{data.bio}</span></p>
+
+    <!-- 
+        if submitting a bio over 260 charc long, 
+        a fail function is called which updates the 
+        page store with a page.form.problem object which we can 
+        listen for here and provide feedback to the user with
+     -->
+    <p>Status Code: {$page.status}</p>
+    <p class="text-error">{$page.form?.problem ?? ""}</p>
+
+    <!-- 
+        Form has submission method of POST instead of on-submit event handler 
+        (which would be entirely client-side)
+        This is becuase this form submission will be sent to the server 
+        to be handled by a svelte kit action
+        Svelte kit action is a convenient way to handle server side code
+
+        In textarea, the value is set to the bio data 
+        we fetch from the server (e.g. data.bio)
+        The name used here is important becuase we'll use it to change 
+        the data on the server as well in +page.server.ts
+
+        If you submit this form without 'use:enhance', it makes 
+        a full page reload when submitted because that is default HTML. 
+        With svelte, you can add 'use:enhance' to the form so that it uses 
+        JavaScript to submit the info and the page is not entirely re-rendered.
+     -->
+    <form method="POST" use:enhance>
+        <div class="form-control">
+            <label for="bio" class="label">
+            <span class="label-text">Your bio</span>
+            </label>
+            <textarea
+                name="bio"
+                class="textarea textarea-bordered textarea-accent"
+                value={data.bio}
+            />
+        </div>
+        <button class="btn btn-primary my-5">Update Bio</button>
+    </form>
+</main>
